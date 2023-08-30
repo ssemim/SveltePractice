e84c9cf5b2286e1a3a715da251474496d2782512
ssemim <82812894+ssemim@users.noreply.github.com>
2023-08-30 14:57:21 +0900

    Create 5.svelte

---
+++ 5.svelte
@@ -0,0 +1,23 @@
+<script>
+
+let text = ''
+	
+</script>
+
+<h1>
+{text}
+</h1>
+
+<input type="text" 
+	value={text}
+<!--  양방향 데이터 바인딩 -->
+		on:input={(e) =>{text = e.target.value}} />
+
+<input type="text"
+	bind:value={text}/> 
+
+<!--위에거나 이거나 똑같음 --> 
+
+	<button on:click={()=>{text='Heropy'}}>
+		Click
+	</button>
