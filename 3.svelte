8bec47b6a40bd5bc18474a38845a6ea8871cea02
ssemim <82812894+ssemim@users.noreply.github.com>
2023-08-30 11:37:25 +0900

    Create 3.svelte
    3번 예제

---
+++ 3.svelte
@@ -0,0 +1,15 @@
+<script>
+   let name = 'world';
+   let toggle = false;
+
+   if(toggle){}
+</script>
+<button on:click={()=>{toggle = !toggle}}>
+   Toggle
+</button>
+
+{#if toggle}
+<h1>Hello {name}!</h1>
+{/if}
+
+  // if 문 사용 가능 : 토글 기능 O 
