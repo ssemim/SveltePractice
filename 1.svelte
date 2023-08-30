9a3d17dcf04287561848073bf4b63c0e08cbf6bd
ssemim <82812894+ssemim@users.noreply.github.com>
2023-08-30 11:34:29 +0900

    Create 1.svelte
    1번 예제

---
+++ 1.svelte
@@ -0,0 +1,31 @@
+<script>
+   let name = 'world';
+   let age = 25
+
+function assign(){
+
+name = 'Heropy'
+   age =36
+   
+}
+   
+</script>
+
+<img src="" alt={name} /> 
+<input type = "text" bind:value="{name}" />
+<h1>Hello {name}!</h1>
+<h2 class={age<85 ? 'active' : ''}>{age}</h2>
+
+<button on:click={assign}>Assign</button>
+
+
+<style>
+
+h1 {
+   color : red;
+}
+.active{
+   color : blue;
+}
+   
+</style>
